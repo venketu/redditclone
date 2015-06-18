ad9fe921e17f6dc722947fa1b4417c91abce8eb4
Michael Weaver <venketu@yahoo.com>
2015-06-18 04:45:46 -0500

    vote

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,18 +1,13 @@
 class LinksController < ApplicationController
   before_action :set_link, only: [:show, :edit, :update, :destroy]
-  before_filter :authenticate_user!, :except => [:index, :show]
+  before_action :authenticate_user!, except: [:index, :show]
   before_action :authorized_user, only: [:edit, :update, :destroy]
+
   # GET /links
   # GET /links.json
   def index
     @links = Link.all
   end
-
-
-def authorized_user
-  @link = current_user.links.find_by(id: params[:id])
-  redirect_to links_path, notice: "Not authorized to edit this link" if @link.nil?
-end
 
   # GET /links/1
   # GET /links/1.json
@@ -21,11 +16,9 @@ end
 
   # GET /links/new
   def new
-  @link = current_user.links.build
+    @link = current_user.links.build
   end
 
-
- 
   # GET /links/1/edit
   def edit
   end
@@ -70,10 +63,27 @@ end
     end
   end
 
+  def upvote
+    @link = Link.find(params[:id])
+    @link.upvote_by current_user
+    redirect_to :back
+  end
+
+  def downvote
+    @link = Link.find(params[:id])
+    @link.downvote_from current_user
+    redirect_to :back
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_link
       @link = Link.find(params[:id])
+    end
+
+    def authorized_user
+      @link = current_user.links.find_by(id: params[:id])
+      redirect_to links_path, notice: "Not authorized to edit this link" if @link.nil?
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
